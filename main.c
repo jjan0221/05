92d99eedbdb8fc405b04c05fcfb263e68eb27035
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-10-08 15:30:18 +0900

    for

--- main.c
+++ main.c
@@ -8,13 +8,9 @@
 int main(int argc, char *argv[]) 
 {
 	
-	int i=0, meter;
+	int i;
 	
-	while (i<3)
-	{
-		meter = i*1609;
-		printf("%d 마일은 %d미터입니다.\n", i, meter);
-		i++;
-	}
+	for(i=0; i<10; i++)
+		printf("hello world!\n");
 	return 0;
 }
